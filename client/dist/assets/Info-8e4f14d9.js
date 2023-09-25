5a948b17821895117be707838f6de5349692e46a
Andres Felipe Rodriguez Baez <andres.rodriguezb@anh.gov.co>
2023-09-25 13:08:18 -0500

    dist added

---
+++ client/dist/assets/Info-8e4f14d9.js
@@ -0,0 +1,1 @@
+import{j as s,a,N as r,b as e}from"./index-3ed353e3.js";const n=()=>s("div",{className:"App",children:[a(r,{}),a("div",{className:"header",children:"Information"}),a(e,{})]});export{n as default};
